26d9b452e5b0f242a34e3eae50c862ffb3a35352
Motirock <117296582+Motirock@users.noreply.github.com>
2023-04-30 09:18:19 -0400

    Delete source/include directory

--- source/include/TextureManager.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-
-class TextureManager {
-public:
-    static SDL_Texture* loadTexture(const char *fileName, SDL_Renderer *renderer);
-};
